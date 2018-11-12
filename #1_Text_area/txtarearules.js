b7ecd2b12f5ceef9c01adaff1272cfc4d4b0de67
PauloPoli <43033395+PauloPoli@users.noreply.github.com>
2018-11-12 23:23:29 +0200

    Update #1_Text_area/txtarearules.js

---
+++ #1_Text_area/txtarearules.js
@@ -0,0 +1,15 @@
+function do_resize(textbox) {
+
+    var maxrows=5; 
+     var txt=textbox.value;
+     var cols=textbox.cols;
+   
+    var arraytxt=txt.split('\n');
+     var rows=arraytxt.length; 
+   
+    for (i=0;i<arraytxt.length;i++) 
+     rows+=parseInt(arraytxt[i].length/cols);
+   
+    if (rows>maxrows) textbox.rows=maxrows;
+     else textbox.rows=rows;
+    }
